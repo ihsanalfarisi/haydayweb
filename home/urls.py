41ce9689bca804c52109b36e0d3a5b8690067ffd
muhammad.ihsan06 <muhammad.ihsan06@ui.ac.id>
2022-05-10 18:38:04 +0700

    home

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
+from django import views
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
 ]
