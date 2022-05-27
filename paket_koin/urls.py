c61fc4268de705b6a0d99bf456b0eda5b758797f
mthariqza <mthariqza@gmail.com>
2022-05-27 23:39:48 +0700

    fix bug

--- paket_koin/urls.py
+++ paket_koin/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('paket_koin/validation', views.create_validation_paketkoin, name='validation'),
     path('transaksi/create/<str:paket_koin>/<str:harga>', views.check_jumlah_transaksi, name='create_beli'),
     path('transaksi/validation/<str:paket_koin>', views.create_validation_transaksi, name='validate'),
-    path('paket_koin/update/<str:paket_koin>', views.check_koin, name='update'),
+    path('paket_koin/update/<str:paket_koin>', views.check_koin, name='updateKoin'),
     path('paket_koin/update/validate/<str:paket_koin>', views.update_koin, name='updatevalidate'),
-    path('paket_koin/delete/<str:paket_koin>', views.delete_koin, name='delete')
+    path('paket_koin/delete/<str:paket_koin>', views.delete_koin, name='deleteKoin')
 ]
